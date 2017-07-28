3e412e4acb840d8e4a40fb4fa2600cb5e5d18100
alex <alexander.alvear@gmail.com>
2017-07-28 14:37:18 +0200

    firts

---
+++ src/adex/Main.java
@@ -0,0 +1,20 @@
+package adex;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) {
+		Queue<Persona> cola = new PriorityQueue<Persona>();
+
+		cola.add(new Persona("Daniel", 1));
+		cola.add(new Persona("Katherine", 3));
+		cola.add(new Persona("Julio", 2));
+		cola.add(new Persona("Maria", 1));
+
+		while (!cola.isEmpty()) {
+			Persona a = cola.remove();
+			System.out.println(a.getNombre() + " " + a.getTipo());
+		}
+	}
+}
